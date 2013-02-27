54ddc00747923843c3950b2f78fe3511cdebd4f3
Valtteri <Valtteri@86.50.87.190>
2013-02-27 15:57:59 +0200

    juu elikkäs

---
+++ src/edu/vserver/exercises/mathpath/PathModel.java
@@ -0,0 +1,65 @@
+package edu.vserver.exercises.mathpath;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class PathModel {
+
+    private ArrayList<Integer> list;
+
+    private int min, max, amountOfOptions;
+
+    private int correctAnswer;
+
+    private static Random rnd = new Random();
+
+    /*
+     * @.pre amountOfOptions > 0
+     */
+    public PathModel(int min, int max, int amountOfOptions) {
+        list = new ArrayList<Integer>();
+        this.min = min;
+        this.max = max;
+        this.amountOfOptions = amountOfOptions;
+        generateAnswers();
+
+    }
+
+    private void generateAnswers() {
+        correctAnswer = rnd.nextInt(max - min + 1) + min;
+        list.add(correctAnswer);
+        generateWrongAnswers(correctAnswer);
+
+        Collections.shuffle(list);
+    }
+
+    private void generateWrongAnswers(int correctAnswer) {
+
+        while (list.size() < amountOfOptions) {
+            int wrongAnswer = rnd.nextInt(max - min + 1) + min;
+            if (wrongAnswer != correctAnswer) {
+                list.add(wrongAnswer);
+            }
+        }
+    }
+
+    public int getOption(int i) {
+        return list.get(i);
+    }
+
+    public int getLength() {
+        return list.size();
+    }
+
+    public int getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void generateNewAnswers() {
+        list.clear();
+
+        generateAnswers();
+    }
+
+}
