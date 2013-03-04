5acc035db4f8c9860b4d0d9ecdf126a3c121a444
Valtteri <Valtteri@86.50.87.190>
2013-03-04 15:25:39 +0200

    juu

---
+++ src/edu/vserver/exercises/mathpath/MathPathEditor.java
@@ -0,0 +1,25 @@
+package edu.vserver.exercises.mathpath;
+
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+
+public class MathPathEditor extends VerticalLayout implements ClickListener {
+
+	private Button save;
+	private Button test;
+	private TextField questionText;
+
+	public MathPathEditor() {
+
+	}
+
+	@Override
+	public void buttonClick(ClickEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
