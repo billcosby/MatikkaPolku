1716650be95c25c77e70b3173cdaf28446a65a68
Valtteri <Valtteri@86.50.87.190>
2013-03-06 17:25:12 +0200

    CSS, notification

--- src/edu/vserver/exercises/mathpath/PathLayout.java
+++ src/edu/vserver/exercises/mathpath/PathLayout.java
@@ -2,122 +2,127 @@ package edu.vserver.exercises.mathpath;
 
 import java.util.ArrayList;
 
+import com.vaadin.server.Page;
+import com.vaadin.shared.Position;
 import com.vaadin.ui.AbsoluteLayout;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
-import com.vaadin.ui.CheckBox;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 
 public class PathLayout extends AbsoluteLayout {
 
-    private static final long serialVersionUID = 1L;
-    private final ArrayList<Button> currentOptions;
-    private Button correctOption;
-    private MathPathExecutor executor;
+	private static final long serialVersionUID = 1L;
+	private final ArrayList<Button> currentOptions;
+	private Button correctOption;
+	private final MathPathExecutor executor;
 
-   
-    private final Button middleButton;
+	private final Button middleButton;
 
-    public PathLayout(MathPathExecutor executor) {
-        super();
+	public PathLayout(MathPathExecutor executor) {
+		super();
 
-        this.executor = executor;
-        currentOptions = new ArrayList<Button>();
+		this.executor = executor;
+		currentOptions = new ArrayList<Button>();
 
-        this.setWidth("100%");
-        this.setHeight("300px");
+		this.setWidth("100%");
+		this.setHeight("300px");
 
-        middleButton = new Button("");
-        middleButton.setStyleName("mathpath-middle");
-        
-        addComponent(middleButton, "top:50%; left:25%");
-        this.setStyleName("path-layout");
-    }
+		middleButton = new Button("");
+		middleButton.setStyleName("mathpath-middle");
 
-    
-    public void addOption(String string) {
-        Button b = new Button(string);
-        b.addStyleName("mathpath-option");
+		addComponent(middleButton, "top:50%; left:25%");
+		this.setStyleName("path-layout");
+	}
 
-        // add a click listener for the new option
-        b.addClickListener(new ClickListener() {
-            @Override
-            public void buttonClick(ClickEvent event) {
+	public void addOption(String string) {
+		Button b = new Button(string);
+		b.addStyleName("mathpath-option");
 
-                if (event.getButton() == correctOption) {
-                    handleCorrectAnswer();
-                } else {
-                    handleWrongAnswer(event.getButton());
-                    
-                }
-            }
+		// add a click listener for the new option
+		b.addClickListener(new ClickListener() {
+			@Override
+			public void buttonClick(ClickEvent event) {
 
-        });
-        currentOptions.add(b);
-        updatePositions();
-    }
+				if (event.getButton() == correctOption) {
+					handleCorrectAnswer();
+				} else {
+					handleWrongAnswer(event.getButton());
 
-    private void handleWrongAnswer(Button button) {
-    	button.setEnabled(false);
-     	
-        Notification.show("Try again!");
-        executor.handleWrongAnswer();
-    }
+				}
+			}
 
-    private void handleCorrectAnswer() {
-        Notification.show("Correct!");
-        executor.handleCorrectAnswer();
-    }
+		});
+		currentOptions.add(b);
+		updatePositions();
+	}
 
-    public void removeOption(Button b) {
-        removeComponent(b);
-        currentOptions.remove(b);
-        updatePositions();
-    }
+	private void handleWrongAnswer(Button button) {
+		button.setEnabled(false);
 
-    private void updatePositions() {
-        // first remove old buttons, so we can reposition (there might be a
-        // better way to do it?)
-        for (Button b : currentOptions) {
-            removeComponent(b);
-        }
+		Notification wrong = new Notification("Try again!", "");
+		// Notification.show("Try again!");
 
-        // calculate the vertical distance between buttons (in css %)
-        int heightIncrement = 100 / (currentOptions.size() + 1);
+		wrong.setDelayMsec(4);
+		wrong.setPosition(Position.MIDDLE_CENTER);
+		wrong.setStyleName("mathpath");
 
-        for (int i = 0; i < currentOptions.size(); i++) {
-            // format the vertical alignment
-            String position = "top:" + heightIncrement * (i + 1)
-                    + "%; right:15%";
+		wrong.show(Page.getCurrent());
+		executor.handleWrongAnswer();
+	}
 
-            addComponent(currentOptions.get(i), position);
+	private void handleCorrectAnswer() {
+		Notification.show("Correct!");
+		executor.handleCorrectAnswer();
+	}
 
-        }
-    }
+	public void removeOption(Button b) {
+		removeComponent(b);
+		currentOptions.remove(b);
+		updatePositions();
+	}
 
-    public void setCorrectOption(String riddle) {
-        // find the correct option button and add a click listener to it
-        for (int i = 0; i < currentOptions.size(); i++) {
-            if (currentOptions.get(i).getCaption().equals(riddle)) {
+	private void updatePositions() {
+		// first remove old buttons, so we can reposition (there might be a
+		// better way to do it?)
+		for (Button b : currentOptions) {
+			removeComponent(b);
+		}
 
-                correctOption = currentOptions.get(i);
+		// calculate the vertical distance between buttons (in css %)
+		int heightIncrement = 100 / (currentOptions.size() + 1);
 
-            }
-        }
-    }
+		for (int i = 0; i < currentOptions.size(); i++) {
+			// format the vertical alignment
+			String position = "top:" + heightIncrement * (i + 1)
+					+ "%; right:15%";
 
-    public void setMiddleCaption(String caption) {
-        middleButton.setCaption(caption);
-    }
+			addComponent(currentOptions.get(i), position);
 
-    public void clearOptions() {
-        for (Button b : currentOptions) {
-            removeComponent(b);
-        }
-        currentOptions.clear();
+		}
+	}
 
-    }
+	public void setCorrectOption(String riddle) {
+		// find the correct option button and add a click listener to it
+		for (int i = 0; i < currentOptions.size(); i++) {
+			if (currentOptions.get(i).getCaption().equals(riddle)) {
+
+				correctOption = currentOptions.get(i);
+
+			}
+		}
+	}
+
+	public void setMiddleCaption(String caption) {
+		middleButton.setCaption(caption);
+	}
+
+	public void clearOptions() {
+		for (Button b : currentOptions) {
+			removeComponent(b);
+		}
+		currentOptions.clear();
+
+	}
 
 }
